4028429539fb280938f5218c74713a4511519dd4
Usama Ansari <usamazansari@outlook.com>
2023-11-26 14:53:41 +0100

    feat: add breadcrumb

--- src/components/react/landing-pages/news/components/News.tsx
+++ src/components/react/landing-pages/news/components/News.tsx
@@ -1,12 +1,32 @@
 import { Provider } from '@landing-pages/react/common/components';
-import { Card, Flex, ScrollArea, Space, Text } from '@mantine/core';
-import { useMemo } from 'react';
+import { Anchor, Breadcrumbs, Card, Flex, ScrollArea, Space, Text } from '@mantine/core';
+import React, { useMemo } from 'react';
 import { useNewsApi } from '../hooks';
 import { NewsLayout } from './NewsLayout';
 import { RelatedPopularNews } from './RelatedPopularNews';
 
 export function News({ apiKey, category = '' }: { apiKey: string; category?: string }) {
   const response = useNewsApi({ apiKey, category });
+
+  const items = useMemo(() => {
+    const items = [] as { title: string; icon: string; href: string }[];
+    items.push({ title: '', icon: 'home', href: '/' });
+    items.push({ title: 'News', icon: '', href: '/news' });
+    if (!category) return items;
+    items.push({ title: category.toUpperCase(), icon: '', href: `/news/${category.toLowerCase().replace(/\s/g, '-')}` });
+    return items;
+  }, [category]);
+
+  const breadCrumbItems = useMemo(() => {
+    return items.map(item => (
+      <Anchor key={item.href} href={item.href} underline="never">
+        <Flex align="center" gap="xs">
+          {!item.icon ? null : <span className="material-icons">{item.icon}</span>}
+          {!item.title ? null : <Text>{item.title}</Text>}
+        </Flex>
+      </Anchor>
+    ));
+  }, [items]);
 
   const relatedPopularCategories = useMemo(() => {
     const categories = response?.data?.news
@@ -28,6 +48,8 @@ export function News({ apiKey, category = '' }: { apiKey: string; category?: str
     <Provider>
       <ScrollArea h="100%">
         <Flex direction="column" gap="lg" className="container mx-auto">
+          <Space />
+          <Breadcrumbs>{breadCrumbItems}</Breadcrumbs>
           <Card className="h-32 grid place-content-center mt-4" withBorder>
             <Text ta="center">Advertisement</Text>
           </Card>
