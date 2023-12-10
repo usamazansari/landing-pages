c913676219e6b9279cebe5b9ca23aea28a64e711
Usama Ansari <usamazansari@outlook.com>
2023-12-10 12:28:03 +0100

    feat: use badges to set category instead of redirect

--- src/components/react/landing-pages/news/components/internal/NewsItem.tsx
+++ src/components/react/landing-pages/news/components/internal/NewsItem.tsx
@@ -1,7 +1,9 @@
 import { Anchor, Badge, Box, Card, Flex, Image, Spoiler, Text } from '@mantine/core';
 import type { INews } from '../../types';
+import { setSelectedCategory, useAppDispatch } from 'src/components/react/landing-pages/news/store';
 
 export function NewsItem({ newsItem, largeArea = false }: { newsItem: INews; largeArea?: boolean }) {
+  const dispatch = useAppDispatch();
   return (
     <Card className="h-full" withBorder>
       <Card.Section>
@@ -12,13 +14,20 @@ export function NewsItem({ newsItem, largeArea = false }: { newsItem: INews; lar
           {newsItem.title}
         </Anchor>
         <Flex align="center" justify="flex-start" gap="md">
-          <Text size="sm" c="dimmed" fs="italic" className="flex items-center gap-xs">
+          <Text size="sm" c="dimmed" fs="italic" className="flex items-center gap-xs whitespace-nowrap">
             <span className="material-icons">today</span>
             {new Date(newsItem.published).toDateString()}
           </Text>
           <Flex align="flex-start" wrap="wrap" gap="xs">
             {newsItem.category.map(c => (
-              <Badge key={c} variant="filled" component="a" href={`/news/${c.toLowerCase().replace(/\s/g, '-')}`} style={{ cursor: 'pointer' }}>
+              <Badge
+                key={c}
+                variant="filled"
+                component="button"
+                onClick={() => {
+                  dispatch(setSelectedCategory(c));
+                }}
+                style={{ cursor: 'pointer' }}>
                 {c.toUpperCase()}
               </Badge>
             ))}
