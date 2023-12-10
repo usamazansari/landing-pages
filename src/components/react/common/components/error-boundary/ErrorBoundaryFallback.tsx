027317d1a27a16c61a9677a0bf11ee0bfa6196bf
Usama Ansari <usamazansari@outlook.com>
2023-12-10 12:31:01 +0100

    fix: error in new page

--- src/components/react/common/components/error-boundary/ErrorBoundaryFallback.tsx
+++ src/components/react/common/components/error-boundary/ErrorBoundaryFallback.tsx
@@ -2,7 +2,7 @@ import { ErrorCard } from '@landing-pages/react/common/components';
 import { Box } from '@mantine/core';
 import React from 'react';
 
-export function ErrorBoundaryFallback({ error }: { error: Error }): React.ReactNode {
+export function ErrorBoundaryFallback({ error }: { error?: Error }): React.ReactNode {
   return (
     <Box className="w-screen grid container mx-auto my-xl">
       <ErrorCard error={error} />
