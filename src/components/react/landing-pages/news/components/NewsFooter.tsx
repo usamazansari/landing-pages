06eaab88b1aedbc0f24fa59b1cc68d82aed26ad0
Usama Ansari <usamazansari@outlook.com>
2023-12-13 22:30:16 +0100

    feat: update footer

--- src/components/react/landing-pages/news/components/NewsFooter.tsx
+++ src/components/react/landing-pages/news/components/NewsFooter.tsx
@@ -4,11 +4,20 @@ export function NewsFooter() {
   return (
     <Box className="grid">
       <Divider />
-      <Flex gap="xs" px="md" justify="flex-end" align="center" className="h-16">
-        <Text>Made by</Text>
-        <Anchor href="https://github.com/usamazansari" target="_blank" underline="hover" className="underline-offset-2 dark:text-color-danger-fg">
-          @usamazansari
-        </Anchor>
+      <Flex gap="xs" px="md" py="lg" justify="space-between" align="center">
+        <Flex gap="xs" align="center">
+          <Text>Powered by</Text>
+          <Anchor href="https://currentsapi.services/en" target="_blank" underline="hover" className="underline-offset-2">
+            Currents API <span className="material-icons text-[1rem]">open_in_new</span>
+          </Anchor>
+        </Flex>
+
+        <Flex gap="xs" px="md" align="center">
+          <Text>Made by</Text>
+          <Anchor href="https://github.com/usamazansari" target="_blank" underline="hover" className="underline-offset-2">
+            @usamazansari
+          </Anchor>
+        </Flex>
       </Flex>
     </Box>
   );
