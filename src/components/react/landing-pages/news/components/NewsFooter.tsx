b5a26f6b927288ce95e5594979ed5a50ff4a108c
Usama Ansari <usamazansari@outlook.com>
2023-12-13 22:33:30 +0100

    feat: update footer

--- src/components/react/landing-pages/news/components/NewsFooter.tsx
+++ src/components/react/landing-pages/news/components/NewsFooter.tsx
@@ -5,11 +5,19 @@ export function NewsFooter() {
     <Box className="grid">
       <Divider />
       <Flex gap="xs" px="md" py="lg" justify="space-between" align="center">
-        <Flex gap="xs" align="center">
-          <Text>Powered by</Text>
-          <Anchor href="https://currentsapi.services/en" target="_blank" underline="hover" className="underline-offset-2">
-            Currents API <span className="material-icons text-[1rem]">open_in_new</span>
-          </Anchor>
+        <Flex gap="xs" direction="column">
+          <Flex gap="xs" align="center">
+            <Text>Powered by</Text>
+            <Anchor href="https://currentsapi.services/en" target="_blank" underline="hover" className="underline-offset-2">
+              Currents API <span className="material-icons text-[1rem]">open_in_new</span>
+            </Anchor>
+          </Flex>
+          <Flex gap="xs" align="center">
+            <Text>Inspired by</Text>
+            <Anchor href="https://www.bbc.com/news" target="_blank" underline="hover" className="underline-offset-2">
+              BBC <span className="material-icons text-[1rem]">open_in_new</span>
+            </Anchor>
+          </Flex>
         </Flex>
 
         <Flex gap="xs" px="md" align="center">
