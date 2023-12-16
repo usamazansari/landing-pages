965c13b9d7b56b1aa8e482dc978dfbb5d4e08c40
Usama Ansari <usamazansari@outlook.com>
2023-12-16 14:55:47 +0100

    chore: use page with affix for home page

--- src/components/react/landing-pages/home/components/HomePage.tsx
+++ src/components/react/landing-pages/home/components/HomePage.tsx
@@ -1,19 +1,8 @@
+import { PageWithAffix } from '@landing-pages/react/common/components';
 import { Home } from './Home';
 import { HomeFooter } from './HomeFooter';
 import { HomeHeader } from './HomeHeader';
 
 export function HomePage() {
-  return (
-    <>
-      <nav className="sticky top-0 left-0 right-0 z-10">
-        <HomeHeader />
-      </nav>
-      <main>
-        <Home />
-      </main>
-      <footer>
-        <HomeFooter />
-      </footer>
-    </>
-  );
+  return <PageWithAffix nav={<HomeHeader />} main={<Home />} footer={<HomeFooter />} />;
 }
