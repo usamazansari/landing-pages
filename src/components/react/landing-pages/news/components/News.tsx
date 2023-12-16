8ceb1f45b44f55ce7a829db9d28ccdd713365a1b
Usama Ansari <usamazansari@outlook.com>
2023-12-16 14:56:14 +0100

    chore: remove affix from news

--- src/components/react/landing-pages/news/components/News.tsx
+++ src/components/react/landing-pages/news/components/News.tsx
@@ -1,5 +1,4 @@
-import { ActionIcon, Affix, Anchor, Box, Breadcrumbs, Button, Card, Flex, ScrollArea, Space, Text, Transition } from '@mantine/core';
-import { useWindowScroll } from '@mantine/hooks';
+import { Anchor, Box, Breadcrumbs, Button, Card, Flex, ScrollArea, Space, Text } from '@mantine/core';
 import { useMemo } from 'react';
 import { setShouldRefetch, useAppDispatch, useAppSelector } from '../store';
 import { NewsLayout, RelatedPopularNews } from './internal';
@@ -8,7 +7,6 @@ export function News() {
   const dispatch = useAppDispatch();
   const category = useAppSelector(state => state.news.selectedCategory);
   const relatedPopularCategories = useAppSelector(state => state.news.relatedPopularCategories);
-  const [scroll, scrollTo] = useWindowScroll();
 
   const items = useMemo(() => {
     const items = [] as { title: string; icon: string; href?: string }[];
@@ -31,62 +29,51 @@ export function News() {
   }, [items]);
 
   return (
-    <>
-      <ScrollArea h="100%">
-        <Flex direction="column" gap="lg" className="container mx-auto my-lg">
-          <Box>
-            <Space />
-            <Flex align="center" justify="space-between">
-              <Breadcrumbs>{breadCrumbItems}</Breadcrumbs>
-              {false && (
-                <Button
-                  variant="subtle"
-                  onClick={() => {
-                    dispatch(setShouldRefetch(true));
-                  }}>
-                  <Flex align="center" gap="sm">
-                    <Text className="leading-[normal] flex items-center">
-                      <span className="material-icons">refresh</span>
-                    </Text>
-                    <Text fw={500} className="leading-[normal] flex items-center">
-                      Refresh
-                    </Text>
-                  </Flex>
-                </Button>
-              )}
-            </Flex>
+    <ScrollArea h="100%">
+      <Flex direction="column" gap="lg" className="container mx-auto my-lg">
+        <Box>
+          <Space />
+          <Flex align="center" justify="space-between">
+            <Breadcrumbs>{breadCrumbItems}</Breadcrumbs>
+            {false && (
+              <Button
+                variant="subtle"
+                onClick={() => {
+                  dispatch(setShouldRefetch(true));
+                }}>
+                <Flex align="center" gap="sm">
+                  <Text className="leading-[normal] flex items-center">
+                    <span className="material-icons">refresh</span>
+                  </Text>
+                  <Text fw={500} className="leading-[normal] flex items-center">
+                    Refresh
+                  </Text>
+                </Flex>
+              </Button>
+            )}
+          </Flex>
+        </Box>
+        <Box>
+          <Card className="h-32 grid place-content-center mt-4" withBorder>
+            <Text ta="center">Advertisement</Text>
+          </Card>
+        </Box>
+        <Box>
+          <NewsLayout />
+        </Box>
+        {!relatedPopularCategories.length ? null : (
+          <Box className="grid gap-md">
+            <Space style={{ margin: '0.5rem 0rem' }} />
+            <Text size="lg" fw={500}>
+              Related News
+            </Text>
+            <Space style={{ margin: '0.5rem 0rem' }} />
+            {relatedPopularCategories.map(c => (
+              <RelatedPopularNews key={c} category={c} />
+            ))}
           </Box>
-          <Box>
-            <Card className="h-32 grid place-content-center mt-4" withBorder>
-              <Text ta="center">Advertisement</Text>
-            </Card>
-          </Box>
-          <Box>
-            <NewsLayout />
-          </Box>
-          {!relatedPopularCategories.length ? null : (
-            <Box className="grid gap-md">
-              <Space style={{ margin: '0.5rem 0rem' }} />
-              <Text size="lg" fw={500}>
-                Related News
-              </Text>
-              <Space style={{ margin: '0.5rem 0rem' }} />
-              {relatedPopularCategories.map(c => (
-                <RelatedPopularNews key={c} category={c} />
-              ))}
-            </Box>
-          )}
-        </Flex>
-      </ScrollArea>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Transition transition="slide-up" mounted={scroll.y > 0}>
-          {transitionStyles => (
-            <ActionIcon size="xl" variant="filled" radius="xl" onClick={() => scrollTo({ y: 0 })} style={transitionStyles}>
-              <span className="material-icons">expand_less</span>
-            </ActionIcon>
-          )}
-        </Transition>
-      </Affix>
-    </>
+        )}
+      </Flex>
+    </ScrollArea>
   );
 }
