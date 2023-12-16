a1035525d12d3f671492a575218a0ae4b868bfff
Usama Ansari <usamazansari@outlook.com>
2023-12-16 14:32:25 +0100

    feat: add scroll on label click style: improve date view in news card

--- src/components/react/landing-pages/news/components/internal/NewsItem.tsx
+++ src/components/react/landing-pages/news/components/internal/NewsItem.tsx
@@ -1,9 +1,12 @@
 import { Anchor, Badge, Box, Card, Flex, Image, Spoiler, Text } from '@mantine/core';
-import type { INews } from '../../types';
+import { useWindowScroll } from '@mantine/hooks';
 import { setSelectedCategory, useAppDispatch } from 'src/components/react/landing-pages/news/store';
+import type { INews } from '../../types';
 
 export function NewsItem({ newsItem, largeArea = false }: { newsItem: INews; largeArea?: boolean }) {
   const dispatch = useAppDispatch();
+  const [, scrollTo] = useWindowScroll();
+
   return (
     <Card className="h-full" withBorder>
       <Card.Section>
@@ -13,11 +16,15 @@ export function NewsItem({ newsItem, largeArea = false }: { newsItem: INews; lar
         <Anchor href={newsItem.url} target="_blank" fw="bold">
           {newsItem.title}
         </Anchor>
-        <Flex align="center" justify="flex-start" gap="md">
-          <Text size="sm" c="dimmed" fs="italic" className="flex items-center gap-xs whitespace-nowrap">
-            <span className="material-icons">today</span>
-            {new Date(newsItem.published).toDateString()}
-          </Text>
+        <Flex align="center" justify="space-between" gap="xs">
+          <Flex align="center" gap="xs">
+            <Text c="dimmed" size="sm" className="leading-[normal] flex items-center">
+              <span className="material-icons">today</span>
+            </Text>
+            <Text c="dimmed" size="sm" fs="italic" fw={500} className="leading-[normal] flex items-center">
+              {new Date(newsItem.published).toDateString()}
+            </Text>
+          </Flex>
           <Flex align="flex-start" wrap="wrap" gap="xs">
             {newsItem.category.map(c => (
               <Badge
@@ -26,6 +33,7 @@ export function NewsItem({ newsItem, largeArea = false }: { newsItem: INews; lar
                 component="button"
                 onClick={() => {
                   dispatch(setSelectedCategory(c));
+                  scrollTo({ y: 0 });
                 }}
                 style={{ cursor: 'pointer' }}>
                 {c.toUpperCase()}
