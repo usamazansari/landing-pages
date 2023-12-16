957eaff4928c1cc72c18252864166d5bb9590465
Usama Ansari <usamazansari@outlook.com>
2023-12-16 22:14:19 +0100

    fix: update tailwind integration type cast

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
+import markdoc from '@astrojs/markdoc';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import markdoc from '@astrojs/markdoc';
+import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind(), markdoc()],
+  integrations: [mdx(), react(), tailwind() as AstroIntegration, markdoc()],
 });
