c4931d482adccc4019d99c9a0bb1c5a92d362751
Usama Ansari <usamazansari@outlook.com>
2023-12-17 22:07:06 +0100

    feat: deploy with vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,16 @@ import markdoc from '@astrojs/markdoc';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import type { AstroIntegration } from 'astro';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), tailwind() as AstroIntegration, markdoc()],
+  integrations: [mdx(), react(), tailwind(), markdoc()],
   output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
