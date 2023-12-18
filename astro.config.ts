6ba82e5d8f5efb9a66d7165ca95d66cf3a0e482e
Usama Ansari <usamazansari@outlook.com>
2023-12-18 21:58:25 +0100

    chore: test empty deployment - ii

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,15 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import markdoc from "@astrojs/markdoc";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import markdoc from '@astrojs/markdoc';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), markdoc(), mdx()],
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  output: 'server',
+  adapter: vercel(),
 });
+
