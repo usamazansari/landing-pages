6af67abd3edea9cdda6764a9fff22b1b0349c7c6
Usama Ansari <usamazansari@outlook.com>
2023-12-18 22:16:16 +0100

    chore: revive root config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,27 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,mdx,tsx}'],
+  darkMode: ['class', '[data-mantine-color-scheme="dark"]'],
+  theme: {
+    colors: {},
+    screens: {
+      xs: '36em',
+      sm: '48em',
+      md: '62em',
+      lg: '75em',
+      xl: '88em',
+    },
+    extend: {
+      spacing: {
+        xs: 'calc(0.75rem * var(--mantine-scale))',
+        sm: 'calc(0.875rem * var(--mantine-scale))',
+        md: 'calc(1rem * var(--mantine-scale))',
+        lg: 'calc(1.125rem * var(--mantine-scale))',
+        xl: 'calc(1.25rem * var(--mantine-scale))',
+      },
+    },
+  },
+  plugins: [typography],
+};
