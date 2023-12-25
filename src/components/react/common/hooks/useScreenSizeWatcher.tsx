e3d7ebf706ef05db41dede7a70baf5477e0fa32f
Usama Ansari <usamazansari@outlook.com>
2023-12-25 13:28:13 +0100

    chore: update line ending in hooks

--- src/components/react/common/hooks/useScreenSizeWatcher.tsx
+++ src/components/react/common/hooks/useScreenSizeWatcher.tsx
@@ -3,4 +3,3 @@ import { type MantineTheme } from '@mantine/core';
 export const useScreenSizeWatcher = ({ theme, width }: { theme: MantineTheme; width: number }) => ({
   isNarrowViewport: width < parseFloat(theme.breakpoints.md.match(/\d+/)?.[0] ?? '48') * theme.scale * 16,
 });
-
