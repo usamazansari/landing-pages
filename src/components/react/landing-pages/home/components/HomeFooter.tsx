897bc0a5e1a9f6bed0b806c96889fd21a8861fa4
Usama Ansari <usamazansari@outlook.com>
2023-12-25 13:28:43 +0100

    style: update home footer

--- src/components/react/landing-pages/home/components/HomeFooter.tsx
+++ src/components/react/landing-pages/home/components/HomeFooter.tsx
@@ -2,14 +2,16 @@ import { Anchor, Box, Divider, Flex, Text } from '@mantine/core';
 
 export function HomeFooter() {
   return (
-    <Box className="grid">
+    <>
       <Divider />
-      <Flex gap="xs" px="md" justify="flex-end" align="center" className="h-16">
-        <Text>Made by</Text>
-        <Anchor href="https://github.com/usamazansari" target="_blank" underline="hover" className="underline-offset-2 dark:text-color-danger-fg">
-          @usamazansari
-        </Anchor>
-      </Flex>
-    </Box>
+      <Box className="grid my-md">
+        <Flex align="center" justify="center" gap="xs">
+          <Text>Made by</Text>
+          <Anchor href="https://github.com/usamazansari" target="_blank" underline="hover" className="underline-offset-2 dark:text-color-danger-fg">
+            @usamazansari
+          </Anchor>
+        </Flex>
+      </Box>
+    </>
   );
 }
