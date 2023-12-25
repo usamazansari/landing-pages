9357ce7789d7cb076ce25362fb716b45e8ead471
Usama Ansari <usamazansari@outlook.com>
2023-12-25 13:04:19 +0100

    feat: deploy with analytics and speed-insights

--- src/components/react/landing-pages/home/components/Home.tsx
+++ src/components/react/landing-pages/home/components/Home.tsx
@@ -1,4 +1,6 @@
 import { Box, Flex, ScrollArea, Title } from '@mantine/core';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { useMemo } from 'react';
 import { HomeCardList } from '../data';
 import { HomeCard } from './HomeCard';
@@ -6,15 +8,19 @@ import { HomeCard } from './HomeCard';
 export function Home() {
   const cards = useMemo(() => HomeCardList, []);
   return (
-    <ScrollArea h="100%">
-      <Flex direction="column" gap="lg" className="container mx-auto my-lg">
-        <Title order={4}>The Many Facets of the Web</Title>
-        <Box className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-xl">
-          {cards.map(card => (
-            <HomeCard key={card.href} card={card} />
-          ))}
-        </Box>
-      </Flex>
-    </ScrollArea>
+    <>
+      <ScrollArea h="100%">
+        <Flex direction="column" gap="lg" className="container mx-auto my-lg">
+          <Title order={4}>The Many Facets of the Web</Title>
+          <Box className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-xl">
+            {cards.map(card => (
+              <HomeCard key={card.href} card={card} />
+            ))}
+          </Box>
+        </Flex>
+      </ScrollArea>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
