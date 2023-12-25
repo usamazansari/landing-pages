9b1540260a8975d384ea715cb1eb86d4266cd335
Usama Ansari <usamazansari@outlook.com>
2023-12-25 13:28:48 +0100

    style: update news footer

--- src/components/react/landing-pages/news/components/NewsFooter.tsx
+++ src/components/react/landing-pages/news/components/NewsFooter.tsx
@@ -2,31 +2,34 @@ import { Anchor, Box, Divider, Flex, Text } from '@mantine/core';
 
 export function NewsFooter() {
   return (
-    <Box className="grid">
+    <>
       <Divider />
-      <Flex gap="xs" px="md" py="lg" justify="space-between" align="center">
-        <Flex direction="column">
-          <Flex gap="xs" align="center">
-            <Text>Powered by</Text>
-            <Anchor href="https://currentsapi.services/en" target="_blank" underline="hover" className="underline-offset-2">
-              Currents API <span className="material-icons text-[1rem]">open_in_new</span>
-            </Anchor>
-          </Flex>
-          <Flex gap="xs" align="center">
-            <Text>Inspired by</Text>
-            <Anchor href="https://www.bbc.com/news" target="_blank" underline="hover" className="underline-offset-2">
-              BBC <span className="material-icons text-[1rem]">open_in_new</span>
-            </Anchor>
-          </Flex>
-        </Flex>
-
-        <Flex gap="xs" px="md" align="center">
+      <Box className="grid my-md gap-md">
+        <Flex align="center" justify="center" gap="xs">
           <Text>Made by</Text>
           <Anchor href="https://github.com/usamazansari" target="_blank" underline="hover" className="underline-offset-2">
             @usamazansari
           </Anchor>
         </Flex>
-      </Flex>
-    </Box>
+        <Flex gap="md" wrap="wrap" align="center" justify="center">
+          <Flex direction="column" align="center">
+            <Text size="xs" c="dimmed">
+              Powered by
+            </Text>
+            <Anchor href="https://currentsapi.services/en" target="_blank" underline="hover" className="underline-offset-2">
+              Currents API <span className="material-icons text-[1rem]">open_in_new</span>
+            </Anchor>
+          </Flex>
+          <Flex direction="column" align="center">
+            <Text size="xs" c="dimmed">
+              Layout inspired by
+            </Text>
+            <Anchor href="https://www.bbc.com/news" target="_blank" underline="hover" className="underline-offset-2">
+              BBC News <span className="material-icons text-[1rem]">open_in_new</span>
+            </Anchor>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
   );
 }
