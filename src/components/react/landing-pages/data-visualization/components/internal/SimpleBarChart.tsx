460fe6fbca939ea8663345775a74847435ff5251
Usama Ansari <usamazansari@outlook.com>
2024-01-07 20:27:38 +0100

    feat: add basic bar chart

--- src/components/react/landing-pages/data-visualization/components/internal/SimpleBarChart.tsx
+++ src/components/react/landing-pages/data-visualization/components/internal/SimpleBarChart.tsx
@@ -1,14 +1,16 @@
-import { useMantineTheme } from '@mantine/core';
+import { rgba, useMantineTheme } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
-import { extent } from 'd3';
-import { useMemo, useRef } from 'react';
+import { axisBottom, axisLeft, extent, rollup, scaleBand, scaleLinear, select, sum } from 'd3';
+import { useEffect, useMemo, useRef } from 'react';
 import type { ChartBoundaries } from '../../types';
 
 export function SimpleBarChart<DataType extends Record<string, string | number>>({
   data,
+  excludeKeyList = [],
   boundaries = { left: 40, right: 10, top: 10, bottom: 40 },
 }: {
   data: DataType[];
+  excludeKeyList?: string[];
   boundaries?: ChartBoundaries;
 }) {
   const { ref: svgRef, width, height } = useElementSize();
@@ -16,29 +18,71 @@ export function SimpleBarChart<DataType extends Record<string, string | number>>
   const gx = useRef<SVGGElement>(null);
   const gy = useRef<SVGGElement>(null);
 
-  const amountDomain = useMemo(() => extent(data.map(d => +d.amount)) as [number, number], [data]);
+  const aggregatedCategories = useMemo(
+    () =>
+      rollup(
+        data.filter(d => !excludeKeyList.includes(d.category as string)),
+        v => sum(v, d => Math.abs(+d.amount)),
+        d => d.category,
+      ),
+    [data],
+  );
 
-  // const xScale = useMemo(
-  //   () =>
-  //     scaleLinear()
-  //       .domain()
-  //       .range([boundaries.left, width - boundaries.right]),
-  //   [boundaries.left, boundaries.right, dateDomain, width],
-  // );
+  const categoriesDomain = useMemo(() => [...aggregatedCategories.keys()] as string[], [aggregatedCategories]);
+  const amountDomain = useMemo(() => extent(aggregatedCategories.values()) as number[], [aggregatedCategories]);
 
-  // const yScale = useMemo(
-  //   () =>
-  //     scaleLinear()
-  //       .domain(amountDomain)
-  //       .range([height - boundaries.bottom, boundaries.top]),
-  //   [amountDomain, boundaries.bottom, boundaries.top, height],
-  // );
+  const xScale = useMemo(
+    () =>
+      scaleBand()
+        .domain(categoriesDomain)
+        .range([boundaries.left, width - boundaries.right]),
+    [categoriesDomain, boundaries.left, boundaries.right, width],
+  );
+
+  const yScale = useMemo(
+    () =>
+      scaleLinear()
+        .domain(amountDomain)
+        .range([height - boundaries.bottom, boundaries.top]),
+    [amountDomain, boundaries.bottom, boundaries.top, height],
+  );
+
+  const bars = useMemo(
+    () =>
+      ([...aggregatedCategories.entries()] as [string, number][]).map(([category, amount]) => ({
+        x: xScale(category),
+        y: yScale(amount),
+        width: xScale.bandwidth() - 2,
+        height: height - boundaries.bottom - yScale(amount),
+        identifier: category.toLowerCase().replace(/\s/g, '-').replace(/-+/g, '-'),
+      })),
+    [aggregatedCategories, boundaries.bottom, height, xScale, yScale],
+  );
+
+  useEffect(() => {
+    select(gx.current).call(axisBottom(xScale) as (s: unknown) => void);
+  }, [gx, xScale]);
+
+  useEffect(() => {
+    select(gy.current).call(axisLeft(yScale) as (s: unknown) => void);
+  }, [gy, yScale]);
 
   return (
-    <svg ref={svgRef} className="w-full h-full">
+    <svg ref={svgRef} className="w-full h-full min-h-96 min-w-full">
       <g ref={gx} transform={`translate(0, ${height - boundaries.bottom})`} />
       <g ref={gy} transform={`translate(${boundaries.left}, 0)`} />
-      {/* <path fill="none" d={d} stroke={theme.colors.blue[5]} /> */}
+      {bars.map(({ x, y, width, height, identifier }) => (
+        <rect
+          key={identifier}
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          // fill={rgba(theme.colors.blue[5], 0.5)}
+          fill={theme.colors.blue[5]}
+          // stroke={theme.colors.blue[5]}
+        />
+      ))}
     </svg>
   );
 }
