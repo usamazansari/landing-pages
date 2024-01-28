daf593f7db61bf7c24ea69dd4a545a1fac038724
Usama Ansari <usamazansari@outlook.com>
2024-01-28 12:30:50 +0100

    fix: potential build fixes

--- src/components/react/landing-pages/data-visualization/store/data-visualization.api.ts
+++ src/components/react/landing-pages/data-visualization/store/data-visualization.api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const dataVisualizationApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '' }),
-  endpoints: builder => ({}),
+  endpoints: () => ({}),
 });
 
 // export const {} = dataVisualizationApi;
