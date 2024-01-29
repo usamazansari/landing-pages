8beb7623eab0bf4d8e24f9306bca6b971e1dcbf7
Usama Ansari <usamazansari@outlook.com>
2024-01-29 20:20:06 +0100

    refactor: remove scrollarea from home page

--- src/components/react/landing-pages/home/components/Home.tsx
+++ src/components/react/landing-pages/home/components/Home.tsx
@@ -1,6 +1,4 @@
-import { Box, Flex, ScrollArea } from '@mantine/core';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Box, Flex } from '@mantine/core';
 import { useMemo } from 'react';
 import { HomeCardList } from '../data';
 import { HomeCard } from './HomeCard';
@@ -9,17 +7,13 @@ export function Home() {
   const cards = useMemo(() => HomeCardList, []);
   return (
     <>
-      <ScrollArea h="100%">
-        <Flex direction="column" gap="lg" className="container mx-auto my-lg" px="md">
-          <Box className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-xl">
-            {cards.map((card) => (
-              <HomeCard key={card.href} card={card} />
-            ))}
-          </Box>
-        </Flex>
-      </ScrollArea>
-      <Analytics />
-      <SpeedInsights />
+      <Flex direction="column" gap="lg" className="container mx-auto my-lg" px="md">
+        <Box className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-xl">
+          {cards.map((card) => (
+            <HomeCard key={card.href} card={card} />
+          ))}
+        </Box>
+      </Flex>
     </>
   );
 }
